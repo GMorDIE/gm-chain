c78a29a4ce0b113b39457b6d9441903f39cfffc3
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2022-09-11 17:07:13 +0200

    Update runtime/src/lib.rs
    Co-authored-by: Gabriel Facco de Arruda <arrudagates@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -174,8 +174,8 @@ impl_opaque_keys! {
 
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("gmordie"),
-    impl_name: create_runtime_str!("gmordie"),
+    spec_name: create_runtime_str!("gm_parachain"),
+    impl_name: create_runtime_str!("gm_parachain"),
     authoring_version: 1,
     spec_version: 6,
     impl_version: 0,
